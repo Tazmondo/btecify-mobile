bde839c6c9658836b93d5c759e46da018bfb5c06
Tazmondo <fondcoolman@outlook.com>
2022-05-03 22:36:49 +0100

    Add alt text to icon
    (unnecessary but IDE warning was annoying)

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,12 +1,12 @@
 import PlayFill from "../assets/images/play-circle-fill.svg"
-import {Platform, } from "react-native";
+import {Platform,} from "react-native";
 import {SvgProps} from "react-native-svg";
 
 // @ts-ignore
 // Use this to avoid bug with web builds. "Incorrect casing" or w/e for svgs
 const Icon = ({ children }) => {
     if (Platform.OS === 'web') {
-        return <img src={children.type} {...children.props} />;
+        return <img src={children.type} {...children.props} alt="An icon"/>;
     }
     return children;
 };

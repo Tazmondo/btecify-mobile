59aa90dbbcb6a60cecf9bc19945f248836e53b07
Tazmondo <fondcoolman@outlook.com>
2022-05-05 23:59:18 +0100

    Add end queue listener

--- controllers/Playlist.ts
+++ controllers/Playlist.ts
@@ -1,8 +1,7 @@
-import TrackPlayer, {Track} from "react-native-track-player";
+import TrackPlayer, {State, Track} from "react-native-track-player";
 import {Song} from "../schemas";
 import {api} from "../constants/Api";
 
-let isPlaying = false
 
 // Polyfill Promise.allSettled
 Promise.allSettled = Promise.allSettled || ((promises: any) => Promise.all(
@@ -31,14 +30,14 @@ export function updatePlaylist(props: playlistInfoType) {
     unbufferedSongs = []
     buffer = []
 
-    if (!isPlaying) {
-        isPlaying = true
-        getNextSongs().then(songs => {
-            console.log("songs", songs);
-            return TrackPlayer.add(songs);
-        })
-            .then(() => TrackPlayer.play())
-    }
+    TrackPlayer.getState().then(state => {
+        if (state != State.Playing) {
+            getNextSongs().then(songs => {
+                return TrackPlayer.add(songs);
+            })
+                .then(() => TrackPlayer.play())
+        }
+    })
 }
 
 function getRandomSongs() {
@@ -87,12 +86,10 @@ export async function getNextSongs(): Promise<Track[]> {
     console.log(infoBuffer, buffer.length)
     let numToAdd = (infoBuffer * 2) - buffer.length
     if (numToAdd > unbufferedSongs.length) {
-        console.log("reducing");
         numToAdd = unbufferedSongs.length
     }
     console.log(numToAdd);
     if (numToAdd > 0) {
-        console.log("adding");
         let newIds = unbufferedSongs.splice(0, numToAdd)
         let newSongs: Track[] = (await Promise.allSettled(newIds.map(id => getSongInfo(id))))
             .map(settled => settled.status == "fulfilled" ? songToTrack(settled.value) : false)
@@ -105,7 +102,6 @@ export async function getNextSongs(): Promise<Track[]> {
     if (response == undefined) {
         response = buffer.splice(0, infoBuffer)
     }
-    console.log("a response", response.length, buffer.length, unbufferedSongs.length);
     return response
 }
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {registerRootComponent} from 'expo';
 import TrackPlayer, {Event} from 'react-native-track-player'
 
 import App from './App';
+import {getNextSongs} from "./controllers/Playlist.js";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
@@ -15,6 +16,13 @@ TrackPlayer.registerPlaybackService(() => {
         TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.destroy());
         TrackPlayer.addEventListener(Event.RemoteNext, () => TrackPlayer.skipToNext());
         TrackPlayer.addEventListener(Event.RemotePrevious, () => TrackPlayer.skipToPrevious());
+
+        TrackPlayer.addEventListener(Event.PlaybackQueueEnded, () => {
+            getNextSongs().then(tracks => {
+                TrackPlayer.add(tracks)
+                TrackPlayer.play()
+            })
+        })
     }
 })
 
